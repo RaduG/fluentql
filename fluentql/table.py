66300c5971ffd3d9dd7862343ccb6ccebb56eefc
Radu Ghitescu <radu.ghitescu@gmail.com>
2020-04-03 19:41:30 +0100

    Create type trees for Column and each column's dtype

--- fluentql/table.py
+++ fluentql/table.py
@@ -1,18 +1,27 @@
-from typing import TypeVar
+from datetime import time, datetime, date
+from typing import Any, Union
+from numbers import Number
 
 
-AnyColumn = TypeVar("AnyColumn")
+AnyType = Any
+StringType = str
+NumberType = Number
+BooleanType = bool
+DateTimeType = datetime
+TimeType = time
+DateType = Union[DateTimeType, date]
+NullType = type(None)
 
 
 class Column:
-    def __init__(self, name, type_=AnyColumn):
+    __dtype__ = AnyType
+
+    def __init__(self, name):
         """
         Args:
             name (str): Column name
-            type_ (type): Column type, defaults to AnyColumn
         """
         self.name = name
-        self.type = type_
         self._alias = None
         self.table = None
 
@@ -45,6 +54,24 @@ class Column:
         self.table = table
         return self
 
+    def equals(self, other):
+        """
+        Compare two Column instances. Two columns are identical when they
+        point to the same Table object, have the same name and have the same type.
+
+        Args:
+            other (Column):
+        
+        Returns:
+            bool
+        """
+        return (
+            isinstance(other, type(self))
+            and other.table is self.table
+            and other.name == self.name
+            and other.type == self.type
+        )
+
     @property
     def is_aliased(self):
         """
@@ -67,23 +94,41 @@ class Column:
         """
         return type(self)(self.name, self.type).bind(self.table)
 
-    def __eq__(self, other):
-        """
-        Compare two Column instances. Two columns are identical when they
-        point to the same Table object, have the same name and have the same type.
 
-        Args:
-            other (Column):
-        
-        Returns:
-            bool
-        """
-        return (
-            isinstance(other, type(self))
-            and other.table is self.table
-            and other.name == self.name
-            and other.type == self.type
-        )
+class AnyColumn(Column):
+    pass
+
+
+class NumberColumn(AnyColumn):
+    __dtype__ = NumberType
+
+
+class BooleanColumn(NumberColumn):
+    __dtype__ = BooleanType
+
+
+class FloatColumn(NumberColumn):
+    pass
+
+
+class IntegerColumn(NumberColumn):
+    pass
+
+
+class StringColumn(AnyColumn):
+    __dtype__ = StringType
+
+
+class DateColumn(AnyColumn):
+    __dtype__ = DateType
+
+
+class DateTimeColumn(DateColumn):
+    __dtype__ = DateTimeType
+
+
+class TimeColumn(DateColumn):
+    __dtype__ = TimeType
 
 
 class Table:
@@ -114,7 +159,7 @@ class Table:
             Column
         """
         if self.__columns__ is None:
-            return Column(name).bind(self)
+            return AnyColumn(name).bind(self)
 
         return self.__columns__[name]
 
