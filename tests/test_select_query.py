d9a5132f01f59ebcb417ee9f79d78415893b3c4c
Radu Ghitescu <radu.ghitescu@gmail.com>
2020-04-13 09:52:30 +0100

    Update test with new APIs

--- tests/test_select_query.py
+++ tests/test_select_query.py
@@ -1,14 +1,14 @@
 import pytest
 
-from fluentql.dialects.base import Dialect
+from fluentql.dialects.generic import GenericSQLDialect
 from fluentql.errors import CompilationError, QueryBuilderError
 from fluentql.query import Query as Q
-from fluentql.table import Table
+from fluentql.types import Table
 
 
 @pytest.fixture
-def dialect():
-    return Dialect(keywords_caps=False)
+def dialect_cls():
+    return GenericSQLDialect
 
 
 @pytest.fixture
@@ -25,67 +25,67 @@ def table_b():
 test_table = Table("test_table")
 
 
-def test_empty_select_query_raises_compile_error(table_a, dialect):
+def test_empty_select_query_raises_compile_error(table_a, dialect_cls):
     with pytest.raises(CompilationError):
-        q = Q.select()
-
         # This should raise a CompilationError
-        dialect.compile(q)
+        Q.select().compile(dialect_cls)
 
 
-def test_select_star(table_a, dialect):
-    q = Q.select().from_(table_a)
+def test_select_star(table_a, dialect_cls):
+    q = Q.select().from_(table_a).compile(dialect_cls)
 
-    assert dialect.compile(q) == "select * from table_a;"
-
-
-def test_select_table_star(table_a, dialect):
-    q = Q.select(table_a.all()).from_(table_a)
-
-    assert dialect.compile(q) == "select table_a.* from table_a;"
+    assert q == "select * from table_a;"
 
 
-def test_column_selection(table_a, dialect):
-    q = Q.select(table_a["col1"], table_a["col2"]).from_(table_a)
+def test_select_table_star(table_a, dialect_cls):
+    q = Q.select(table_a.all()).from_(table_a).compile(dialect_cls)
 
-    assert dialect.compile(q) == "select table_a.col1, table_a.col2 from table_a;"
+    assert q == "select table_a.* from table_a;"
 
 
-def test_column_selection_alias(table_a, dialect):
-    q = Q.select(table_a["col1"].alias("col1_alias")).from_(table_a)
+def test_column_selection(table_a, dialect_cls):
+    q = Q.select(table_a["col1"], table_a["col2"]).from_(table_a).compile(dialect_cls)
 
-    assert dialect.compile(q) == "select table_a.col1 as col1_alias from table_a;"
+    assert q == "select col1, col2 from table_a;"
+
+
+def test_column_selection_alias(table_a, dialect_cls):
+    q = (
+        Q.select(table_a["col1"].alias("col1_alias"))
+        .from_(table_a)
+        .compile(dialect_cls)
+    )
+
+    assert q == "select col1 as col1_alias from table_a;"
 
 
 @pytest.mark.parametrize(
     ["q", "expected"],
     [
         (
-            Q.select().from_(test_table).where(test_table["col1"], "=", 120),
-            "select * from test_table where test_table.col1 = 120;",
+            Q.select().from_(test_table).where(test_table["col1"] == 120),
+            "select * from test_table where col1 = 120;",
         ),
         (
-            Q.select().from_(test_table).where(120, "=", test_table["col1"]),
-            "select * from test_table where 120 = test_table.col1;",
+            Q.select().from_(test_table).where(120 == test_table["col1"]),
+            "select * from test_table where col1 = 120;",
         ),
         (
-            Q.select().from_(test_table).where(test_table["col1"], "like", "%abc"),
-            "select * from test_table where test_table.col1 like '%abc';",
+            Q.select().from_(test_table).where(test_table["col1"].like("%abc")),
+            "select * from test_table where col1 like '%abc';",
         ),
         (
-            Q.select()
-            .from_(test_table)
-            .where(test_table["col1"], "<", test_table["col2"]),
-            "select * from test_table where test_table.col1 < test_table.col2;",
+            Q.select().from_(test_table).where(test_table["col1"] < test_table["col2"]),
+            "select * from test_table where col1 < col2;",
         ),
         (
-            Q.select().from_(test_table).where(1, "=", 1),
-            "select * from test_table where 1 = 1;",
+            Q.select().from_(test_table).where(1 == 1),
+            "select * from test_table where true;",
         ),
     ],
 )
-def test_simple_where_query(q, expected, dialect):
-    assert dialect.compile(q) == expected
+def test_simple_where_query(q, expected, dialect_cls):
+    assert q.compile(dialect_cls) == expected
 
 
 @pytest.mark.parametrize(
@@ -94,29 +94,29 @@ def test_simple_where_query(q, expected, dialect):
         (
             Q.select()
             .from_(test_table)
-            .where(test_table["col1"], "<", 20)
-            .and_where(test_table["col2"], "is", True),
-            "select * from test_table where test_table.col1 < 20 and test_table.col2 is True;",
+            .where(test_table["col1"] < 20)
+            .and_where(test_table["col2"] == True),
+            "select * from test_table where col1 < 20 and col2 = true;",
         ),
         (
             Q.select()
             .from_(test_table)
-            .where(test_table["col1"], "<", 20)
-            .or_where(test_table["col2"], "is", False),
-            "select * from test_table where test_table.col1 < 20 or test_table.col2 is False;",
+            .where(test_table["col1"] < 20)
+            .or_where(test_table["col2"] == False),
+            "select * from test_table where col1 < 20 or col2 = false;",
         ),
         (
             Q.select()
             .from_(test_table)
-            .where(test_table["col1"], "=", "v")
-            .and_where(test_table["col2"], "<", test_table["col3"])
-            .or_where(20, ">", test_table["col5"]),
-            "select * from test_table where test_table.col1 = 'v' and test_table.col2 < test_table.col3 or 20 > test_table.col5;",
+            .where(test_table["col1"] == "v")
+            .and_where(test_table["col2"] < test_table["col3"])
+            .or_where(20 > test_table["col5"]),
+            "select * from test_table where col1 = 'v' and col2 < col3 or col5 < 20;",
         ),
     ],
 )
-def test_compound_where_query(q, expected, dialect):
-    assert dialect.compile(q) == expected
+def test_compound_where_query(q, expected, dialect_cls):
+    assert q.compile(dialect_cls) == expected
 
 
 @pytest.mark.parametrize(
@@ -125,15 +125,15 @@ def test_compound_where_query(q, expected, dialect):
         (
             Q.select()
             .from_(test_table)
-            .where(1, "=", 1)
+            .where(1 == 1)
             .and_where(
-                lambda q: q.where(test_table["col1"], "=", 7).or_where(
-                    test_table["col3"], "=", test_table["col4"]
+                lambda q: q.where(test_table["col1"] == 7).or_where(
+                    test_table["col3"] == test_table["col4"]
                 )
             ),
-            "select * from test_table where 1 = 1 and (test_table.col1 = 7 or test_table.col3 = test_table.col4);",
+            "select * from test_table where true and (col1 = 7 or col3 = col4);",
         )
     ],
 )
-def test_complex_where_query(q, expected, dialect):
-    assert dialect.compile(q) == expected
+def test_complex_where_query(q, expected, dialect_cls):
+    assert q.compile(dialect_cls) == expected
